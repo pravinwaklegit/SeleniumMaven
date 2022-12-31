e619034f3b2f4ac710f960da5244c2f12c60b61c
pravinwgit <pravin.wakle88@hotmail.com>
2022-12-31 22:23:09 +0530

    Added TC04_Test and commiting on branch Branch_pravingit

---
+++ src/test/java/com/practiceSelenium/TC04_Test.java
@@ -0,0 +1,33 @@
+package com.practiceSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC04_Test {
+		
+	    @Test
+		public void Login_test() throws InterruptedException
+		{
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			
+			driver.get("https://opensource-demo.orangehrmlive.com/");
+			
+			 Thread.sleep(3000);
+		     driver.findElement(By.name("username")).sendKeys("Admin");
+			 driver.findElement(By.name("password")).sendKeys("admin123");
+			 driver.findElement(By.xpath("//button [@type= 'submit']")).click();
+			 
+			 Thread.sleep(3000);
+			 Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+			 driver.close();
+			
+		}
+
+	}
